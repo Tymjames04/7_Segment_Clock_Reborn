294078a82e17cdf5170a6ab9f0b2db26a4fda979
Chompinz <144393451+Chompinz@users.noreply.github.com>
2024-12-05 16:56:25 -0600

    Created code to drive 4 piece 7-seg display and display the current time.

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,42 @@
 #include <Arduino.h>
 #include <SPI.h>
-#include "RTClib.h"
+#include <RTClib.h>
+#include <SevSeg.h>
 
+SevSeg sevseg1; 
 RTC_DS1307 rtc;
-
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+int tot_time;
 
 void setup () {
-  Serial.begin(9600);
+  byte numDigits = 4;
+  byte digitPins[] = {2, 3, 4, 5};
+  byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
+  bool resistorsOnSegments = false; // 'false' means resistors are on digit pins
+  byte hardwareConfig = COMMON_CATHODE; // See README.md for options
+  bool updateWithDelays = false; // Default 'false' is Recommended
+  bool leadingZeros = false; // Use 'true' if you'd like to keep the leading zeros
+  bool disableDecPoint = false; // Use 'true' if your decimal point doesn't exist or isn't connected
+  sevseg1.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments,
+  updateWithDelays, leadingZeros, disableDecPoint);
+  sevseg1.setBrightness(90); 
 
-#ifndef ESP8266
-  while (!Serial);
-#endif
-
-if (! rtc.begin()) {
-  Serial.println("Couldn't find RTC");
-  Serial.flush();
-  while (1) delay(10);
+  if (!rtc.begin()) {
+    Serial.flush();
+    while (1) delay(10);
   }
 
-if (! rtc.isrunning()) {
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-}
-
-rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  if (!rtc.isrunning()) {
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
 }
 
 void loop () {
     DateTime now = rtc.now();
-    
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.day(), DEC);
-    Serial.print(" (");
-    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-    Serial.print(") ");
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
+    int cur_time_min = now.minute();
+    int cur_time_hour = now.hour()*100;
+    tot_time = cur_time_min + cur_time_hour;
+  
+    sevseg1.setNumber(tot_time);
+    sevseg1.refreshDisplay();
     Serial.println();
-
-    Serial.println();
-    delay(3000);
 }
